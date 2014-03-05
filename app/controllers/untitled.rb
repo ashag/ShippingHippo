bb9cfd06702dc5315ea84475cc26163886f0d3e2
Agolveo <agolveo@gmail.com>
2014-03-04 21:22:45 -0800

    deleted more files not being used

--- app/controllers/untitled.rb
+++
@@ -1,5 +0,0 @@
-request = {:origin => {:country => 'US', :state => 'CA', :city => 'Beverly Hills', :zip => '90210'}, 
-:destination => {:country => 'CA', :state => 'ON', :city => 'Ottawa', :zip => 'K1P 1J1'}, 
-:package => {:weight => 70, :height => 15, :depth => 10, :length => 4 }
-
-['100', [93,10], :cylinder => true] }
