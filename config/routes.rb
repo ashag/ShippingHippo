f5f6533c4a33d77bb85264fe49674b3552b603a4
Agolveo <agolveo@gmail.com>
2014-03-07 10:23:23 -0800

    changed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ShippingHippo::Application.routes.draw do
 
   post 'rates'        => 'rate_requests#carrier_request'
-  root 'homepage'     => 'rate_requests#homepage'
+  root 'rate_requests#homepage'
   
 end
